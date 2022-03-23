c577585a547f4d7b6a4cdaccf4d7bd571f43a686
amir kurniawan <a1@example.com>
2022-03-23 12:42:09 +0700

    first commit

---
+++ src/lib/header.svelte
@@ -0,0 +1,24 @@
+<script>
+    export let title;
+</script>
+
+<header>
+    <h1><a href="/">{title}</a></h1>
+</header>
+
+
+<style lang="scss">
+    header {
+    background-color: aquamarine;
+    padding: 15px;
+
+    h1 {
+      margin: 0;
+
+      a {
+        color: purple;
+        text-decoration: none;
+      }
+    }
+  }
+</style>
